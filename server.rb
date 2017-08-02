4cd885a59ad0d526749f94a9d7266f1549e9374e
Lubaina R <lubaina.rangwala@gmail.com>
2017-08-03 00:06:05 +0530

    edits from emily + server

--- server.rb
+++ server.rb
@@ -7,7 +7,7 @@ server = WEBrick::HTTPServer.new Port: 8000
 trap 'INT' do server.shutdown end
 
 server.mount_proc '/' do |req, res|
-  res.body = File.read("release-email-01-header.html") + "\n" + File.read("release-email-02-body.html") + "\n" + File.read("release-email-03-footer.html")
+  res.body = File.read("_includes/release-email-01-header.html") + "\n" + File.read("release-email-02-body.html") + "\n" + File.read("_includes/release-email-03-footer.html")
 end
 
 server.start
